145bddd0846ea2528b7e24100b3e235595770711
Ian <iandoesallthethings@gmail.com>
2022-08-18 14:54:42 -0400

    Add wco-ad-remover.js.

---
+++ wco-ad-remover.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         WCO Ad Remover
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Autoclicks the remove ads button on WCOstream
+// @author       Ian Edwards
+// @match        https://www.wcostream.com/*
+// @icon         https://www.google.com/s2/favicons?domain=wcostream.com
+// @grant        none
+// ==/UserScript==
+
+function clickButton() {
+	const button = document.getElementById('anti-ad-close')
+	if (button !== null) button.click()
+}
+
+setTimeout(clickButton, 1000)
